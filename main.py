4cd99a7a7e84eda4509ef5942c5c0148ecec44ae
dovle <dovlet320@gmail.com>
2023-05-21 15:10:46 +0300

    Some update

--- main.py
+++ main.py
@@ -21,20 +21,25 @@ for cred in credentials:
 # locust -f locustfile.py --web-port=8089
 # locust -f locustfile_2.py --web-port=8090
 
+# Базовый класс задач для поведения, предоставляет методы для получения заголовков и хоста
 class BehaviorBase(TaskSet):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    # Метод для получения заголовков запроса
     def _get_headers(self):
         return {"Authorization": f'Bearer {self.user.environment.token}'}
 
+    # Метод для получения хоста
     def _get_host(self):
         return self.user.environment.host
 
 
+# Пример последовательного набора задач
 class YourSequentialTaskSetExample(SequentialTaskSet, BehaviorBase):
-    topic_id = None
+    topic_id = None  # ID темы, который будет задан в UC01_01_01_your_get_example
 
+    # Задача на получение темы
     @task
     def UC01_01_01_your_get_example(self):
         if self.user.environment.token:
@@ -47,7 +52,7 @@ class YourSequentialTaskSetExample(SequentialTaskSet, BehaviorBase):
                 if response.status_code == 200:
                     try:
                         data = response.json()
-                        self.topic_id = data["topicsStat"][0]["topicId"]
+                        self.topic_id = data["topicsStat"][0]["topicId"]  # получаем ID первой темы
                     except ValueError:
                         response.failure("JSON parsing error")
                 else:
@@ -55,7 +60,7 @@ class YourSequentialTaskSetExample(SequentialTaskSet, BehaviorBase):
 
     """
     Показываю пример ответа на запрос, который отправили выше, чтобы было понятно, как его парсили:
-    
+
     {    
         "topicsStat": [{
         "topicId": 0001,
@@ -72,13 +77,14 @@ class YourSequentialTaskSetExample(SequentialTaskSet, BehaviorBase):
     }
     """
 
+    # Задача на отправку пост-запроса
     @task
     def UC01_01_02_your_post_example(self):
         if self.user.environment.token:
             headers = self._get_headers()
             headers["Content-Type"] = "application/json"
             request_data = {
-                "topicId": self.topic_id,
+                "topicId": self.topic_id,  # используем ID темы, полученный в UC01_01_01_your_get_example
             }
             with self.client.post(f"/api/v1/user/topic/plan", data=json.dumps(request_data), headers=headers,
                                   verify=False, catch_response=True,
@@ -103,8 +109,10 @@ class YourSequentialTaskSetExample(SequentialTaskSet, BehaviorBase):
     """
 
 
+# Пример случайного набора задач
 class YourRandomTaskSetExample(BehaviorBase):
 
+    # Задачи с различными весами
     @task(10)
     def UC01_02_01_your_task(self):
         if self.user.environment.token:
@@ -124,19 +132,22 @@ class YourRandomTaskSetExample(BehaviorBase):
                     response.failure(f"Unexpected status code: {response.status_code}. Response: {response.text}")
 
 
+# Смешанное поведение пользователя
 class MixedBehavior(HttpUser):
-    host = "https://yourhost.com"
-    wait_time = between(1, 5)
+    host = "https://yourhost.com"  # хост
+    wait_time = between(1, 5)  # время ожидания между задачами
 
+    # Пропорции задач
     tasks = {YourSequentialTaskSetExample: 45, YourRandomTaskSetExample: 100}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.username, self.password = None, None
+        self.username, self.password = None, None  # учетные данные пользователя
 
+    # Метод, выполняющийся при старте теста
     def on_start(self):
         if self.username is None:
-            self.username, self.password = credentials_queue.get()
+            self.username, self.password = credentials_queue.get()  # получаем учетные данные из очереди
         self.environment.host = self.host
         headers = {"Content-Type": "application/json"}
         request_data = {
@@ -147,15 +158,16 @@ class MixedBehavior(HttpUser):
                               catch_response=True, name="UC01 Login") as response:
             if response.status_code == 200:
                 data = response.json()
-                self.environment.token = data.get("accessToken")
+                self.environment.token = data.get("accessToken")  # сохраняем токен доступа
 
+    # Метод, выполняющийся при завершении теста
     def on_stop(self):
         if self.environment.token:
             headers = {"Authorization": f'Bearer {self.environment.token}'}
-            with self.client.delete("/api/logout", header=headers, headers=headers, verify=False,
+            with self.client.delete("/api/logout", headers=headers, verify=False,
                                     catch_response=True, name="UC01 Logout") as response:
                 if response.status_code == 200:
-                    self.environment.token = None
+                    self.environment.token = None  # удаляем токен доступа
                     if self.username is not None:
-                        credentials_queue.put((self.username, self.password))
+                        credentials_queue.put((self.username, self.password))  # возвращаем учетные данные в очередь
                         self.username, self.password = None, None
